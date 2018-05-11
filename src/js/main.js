727f65d770430b9888096e0d51763098cb2c8528
unknown <velichko.vega@gmail.com>
2018-05-11 14:55:24 +0300

    final

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
 //= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/typed.js/lib/typed.min.js
+
+window.onload = function() {
+    var typed = new Typed(".element", {
+        // Waits 1000ms after typing "First"
+        strings: ["BE SURE YOU PUT YOUR FEET"],
+        showCursor: false,
+        onComplete: function (self) {
+            new Typed(".in-the", {
+                // Waits 1000ms after typing "First"
+                strings: ["in the right place."],
+                showCursor: false
+              });
+        }
+      });
+}
+
+
+  
